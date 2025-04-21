2c44cac1b781f9d04c22a8a2778de9cacc6e58bd
dust6811 <pavel5041p@gmail.com>
2025-04-22 02:17:17 +0300

    getAdress and withdraw

--- ENS-domain.sol
+++ ENS-domain.sol
@@ -1,20 +1,37 @@
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-contract MyFirstContract {
-    struct DomainInfo {
-        address owner;
-        uint256 creationTime;
-        uint256 price;
-    }
-    
+contract DomainRegistry {
     mapping(string => DomainInfo) public domains;
+    address public owner;
+
+    struct DomainInfo {
+        address owner; // Владелец домена
+        uint creationTime; // Время создания домена
+        uint price; // Цена регистрации домена
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not the owner");
+        _;
+    }
 
     function register(string memory name) public payable {
-        require(domains[name].owner == address(0), "Domain alredy registered")
+        require(domains[name].owner == address(0), "Domain already registered");
         domains[name] = DomainInfo(msg.sender, block.timestamp, msg.value);
     }
-    
-    constructor() {
-        
+
+    function getAddress(string memory name) public view returns (address) {
+        return domains[name].owner;
+    }
+
+    function withdraw() public onlyOwner {
+        uint balance = address(this).balance;
+        require(balance > 0, "No funds to withdraw");
+        payable(owner).transfer(balance);
     }
 }
